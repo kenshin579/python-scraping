46dc058bbb20b88b32079a54d07894d1b2513291
Administrator <user@example.com>
2017-07-12 23:22:24 +0900

    finish studying ch7

--- chapter7/1-2grams.py
+++ chapter7/1-2grams.py
@@ -1,7 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-
+"""
+두개의 단위로 자름 
+"""
 def getNgrams(input, n):
     input = input.split(' ')
     output = []
@@ -16,3 +18,13 @@ content = bsObj.find("div", {"id": "mw-content-text"}).get_text()
 ngrams = getNgrams(content, 2)
 print(ngrams)
 print("2-grams count is: " + str(len(ngrams)))
+
+"""
+문제점: 불필요한 부분이 많음. 
+
+[['\nPython\n\n\n\n\nParadigm\nmulti-paradigm:', 'object-oriented,'], ['object-oriented,', 'imperative,'], ['imperative,', 'functional,'], 
+['functional,', 'procedural,'], ['procedural,', 'reflective\n\n\nDesigned\xa0by\nGuido'], ['reflective\n\n\nDesigned\xa0by\nGuido', 'van'], 
+['van', 'Rossum\n\n\nDeveloper\nPython'], ['Rossum\n\n\nDeveloper\nPython', 'Software'], ['Software', 'Foundation\n\n\nFirst\xa0appeared\n20\xa0February'],
+...
+
+"""
