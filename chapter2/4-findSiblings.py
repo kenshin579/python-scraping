e2d950690661f0ae4a0feb35d5bc9b94f5f335de
ykoh <kenshin579@gmail.com>
2018-07-01 10:28:59 +0900

    추가 예제

--- chapter2/4-findSiblings.py
+++ chapter2/4-findSiblings.py
@@ -6,3 +6,14 @@ bsObj = BeautifulSoup(html, "html.parser")
 
 for sibling in bsObj.find("table", {"id": "giftList"}).tr.next_siblings:
     print(sibling)
+
+'''
+추가 작업
+table의 행 별로 원하는 td 값을 프린트함 (ex. Cost)
+https://stackoverflow.com/questions/43096220/how-to-iterate-over-td-tags-using-bs4
+'''
+rows = iter(bsObj.find("table").find_all("tr"))
+next(rows)
+for row in rows:
+    cells = row.find_all("td")
+    print(cells[2].get_text())
