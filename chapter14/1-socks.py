d01adf6a89dccdeb0f49d24fc565d9e3a8562d4d
ykoh <kenshin579@gmail.com>
2018-07-07 18:19:17 +0900

    코멘트 추가

--- chapter14/1-socks.py
+++ chapter14/1-socks.py
@@ -2,6 +2,17 @@ import socks
 import socket
 from urllib.request import urlopen
 
-socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9150)
+socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9050)
 socket.socket = socks.socksocket
 print(urlopen('http://icanhazip.com').read())
+
+'''
+Tor 설치
+># brew install tor 
+
+https://kremalicious.com/simple-tor-setup-on-mac-os-x/
+
+Running Tor
+># tor.sh
+
+'''
